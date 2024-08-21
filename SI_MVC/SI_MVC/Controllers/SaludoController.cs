4b099046ced9c5f444620ff793337cc83f8b643a
Daniel Bustamante <vtdanielf@gmail.com>
2024-08-20 22:20:12 -0500

    Ejercicios de formularios,hace falta desde el 2.3.5

---
+++ SI_MVC/SI_MVC/Controllers/SaludoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SI_MVC.Controllers
+{
+    public class SaludoController : Controller
+    {
+        public IActionResult Index()
+        {
+            string mensaje = " creacion de una pagina que te resuelve la vida xd";
+            ViewBag.mensajeSaludo = mensaje;
+            return View("Index");
+        }
+    }
+}
---
+++ SI_MVC/SI_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SI_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
