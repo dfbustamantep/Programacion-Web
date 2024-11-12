6a6fa867405d873cfdead9b60e9dc8e9e36cbd71
Daniel Bustamante <vtdanielf@gmail.com>
2024-11-11 22:44:00 -0500

    Delegados y funciones Lambda en ASP.NET.CORE

--- Segundo-corte/Delegados_Lambda/Controllers/DelegadosGenericosController.cs
+++ Segundo-corte/Delegados_Lambda/Controllers/DelegadosGenericosController.cs
@@ -4,14 +4,16 @@ namespace Delegados_Lambda.Controllers
 {
     public class DelegadosGenericosController : Controller
     {
-        //Delegado generico que recibe dos parametros
-        public delegate T DelegadoMultiplicacion<T>(T a,T b);
-        //Creamos una instancia estatica
-        public static DelegadoMultiplicacion<double> MultiplicarNumeros = Multiplicacion;
+		//Delegado generico que recibe dos parametros
+		public delegate T DelegadoMultiplicacion<T>(T a, T b);
+		//Creamos una instancia estatica
+		public static DelegadoMultiplicacion<double> MultiplicarNumeros = Multiplicacion;
 
-        //implementamos la funcion estatatica
-        public static double Multiplicacion(double primero, double segundo)
+		//public static Func<double, double, double> MultiplicarNumeros = Multiplicacion;
+		//implementamos la funcion estatatica
+		public static double Multiplicacion(double primero, double segundo)
         {
+            //Console.WriteLine(primero * segundo);
             return primero * segundo;
         }
 
@@ -25,6 +27,7 @@ namespace Delegados_Lambda.Controllers
         [HttpPost]
         public IActionResult CalcularProducto(double primero, double segundo)
         {
+            DelegadoMultiplicacion<double> MultiplicarNumeros = (a, b) => a * b;
             double resultado = MultiplicarNumeros(primero, segundo);
             ViewBag.Producto = resultado;
             return View("DelegadosT");
--- Segundo-corte/Delegados_Lambda/obj/Debug/net8.0/Delegados_Lambda.AssemblyInfo.cs
+++ Segundo-corte/Delegados_Lambda/obj/Debug/net8.0/Delegados_Lambda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delegados_Lambda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e245e4329e2484ff258cd1392badd2d910ecc65f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delegados_Lambda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delegados_Lambda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
